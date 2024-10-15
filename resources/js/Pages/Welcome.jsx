1e68d06a968ecaab67c0bfbb5567673879159b4c
Jonathan Lopez <jlopez0313@hotmail.com>
2024-10-15 08:45:10 -0500

    tenant created listener

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -69,7 +69,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     <li>
                                         {" "}
                                         <a
-                                            href="#"
+                                            href="/dashboard"
                                             className="text-center block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                                             onClick={() => setShow( false )}
                                         >
